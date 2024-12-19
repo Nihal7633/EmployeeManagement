e063f251d4706dda925f8a7b31d856536b77ec53
Nihal7633 <160621811+Nihal7633@users.noreply.github.com>
2024-12-19 10:30:01 +0530

    Update EmployeeController.java

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.Employee;
 import com.example.demo.service.EmployeeService;
 
-@Controller
+@CrossOrigin("*")
 @RestController
 public class EmployeeController {
 	
